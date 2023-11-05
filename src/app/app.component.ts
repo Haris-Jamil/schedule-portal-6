465c7156d0f962622692a1f936cf2a76290a5672
haris.jamil <haris.jamil@colwiz.com>
2023-11-05 14:56:13 +0500

    disabled idle watcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent implements OnInit {
         this.isLogedIn = this.loginService.isLogin();   
       })
     this.init();
-    this.startIdleWatcher();
+    // this.startIdleWatcher();
   }  
 
   init(): void {
